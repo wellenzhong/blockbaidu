0dd4ee70acdd056fcd0b248680040e372ce70ebc
彭彭彭 <binbin@pengpengpengdeMacBook-Air.local>
2017-07-01 01:15:31 +0800

    modified:   block.js 	modified:   manifest.json

--- block.js
+++ block.js
@@ -11,38 +11,25 @@
  	init: function () {
  		this.blockFn();
  	},
+ 	replaceHtml: "<p style='font-size:12px;border:1px solid #eee;'>已为您屏蔽一条来自百度的垃圾信息！</p>",
  	blockFn:function(){
  		var _host=window.location.host;
- 		var _bing,_google;
 		if(_host.indexOf('bing.com')>=0){
-			_bing=true;
-			_google=false;
-		}else {
-			_bing=false;
-			_google=true;
-		}
-		if (_bing) {
 			var _baiduRes=document.getElementsByTagName('cite');
 			for (let i = 0; i < _baiduRes.length; i++) {
 				if (_baiduRes[i].innerHTML.indexOf('baidu')>=0||_baiduRes[i].innerText.indexOf('baidu')>=0) {
-					if (_bing) {
 					this.bingDoit(_baiduRes[i],'li')
-					}else{
-
-					this.ggDoit(_baiduRes[i],'g')
-					}
 					i=i-1;
 				}
 			}
-
-		}else{
+		}else if(_host.indexOf('google.com')>=0){
+			var that=this;
 			//由于google由ajax请求生成的页面，不能用DOMContentLoaded事件，需要监听页面元素变化的MutationObserver事件
 			var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
 			var target = document.body;
 			// 创建观察者对象
 			
 			var observer = new MutationObserver(function(mutations) {
-				
 					mutations.forEach(function(mutation) {
 						if (mutation.addedNodes && (mutation.addedNodes.length > 0)) {
 						var _res=document.getElementById('rso');
@@ -51,21 +38,33 @@
 							for (var i = 0; i < _cite.length; i++) {
 								if (_cite[i].innerHTML.indexOf('baidu')>=0) {
 									// $(_cite[i]).closest('.g').css({'opacity':'0','height':'0'});
-									$(_cite[i]).closest('.g').html("<p style='font-size:12px;border:1px solid #eee;'>已为您屏蔽一条来自百度的垃圾信息！</p>");
+									$(_cite[i]).closest('.g').html(that.replaceHtml);
 								}
 							}
 						}
 					}
 					}); 
-				
-			     
-			});
+				});
 
 			var config = { 
 				childList: true,
 				subtree: true
 			 }
 			observer.observe(target, config);
+		}else if(_host.indexOf('baidu.com')>=0){
+			var that=this;
+			var _bdRes=$('.c-showurl');
+			var _bdResTb=$('.c-showurl-color');
+			_bdRes.map(function () {
+				if ($(this).text().indexOf('baidu')>=0) {
+					$(this).closest('.c-container').html(that.replaceHtml);
+				}
+			})
+			_bdResTb.map(function () {
+				if ($(this).text().indexOf('baidu')>=0) {
+					$(this).closest('.c-container').html(that.replaceHtml);
+				}
+			})
 		}
  	},
  	bingDoit:function(a,b){
@@ -75,7 +74,7 @@
  		while(_node.nodeName!=_b){
  			_node=_node.parentNode
  			if (_node.nodeName==_b) {
- 				_node.innerHTML="<p style='font-size:12px;border:1px solid #eee;'>已为您屏蔽一条来自百度的垃圾信息！</p>"
+ 				_node.innerHTML=this.replaceHtml;
  			}
  		}
  	}
